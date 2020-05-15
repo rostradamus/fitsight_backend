81335ca846a2ba190f975b5b63de3c31835965f4
rostradamus <rolee0429@gmail.com>
2020-05-15 09:24:39 -0700

    Add getUser

--- src/main/java/com/rostradamus/wologbackend/controller/UserController.java
+++ src/main/java/com/rostradamus/wologbackend/controller/UserController.java
@@ -1,14 +1,14 @@
 package com.rostradamus.wologbackend.controller;
 
+import com.rostradamus.wologbackend.model.User;
 import com.rostradamus.wologbackend.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -22,5 +22,13 @@ public class UserController {
   @PreAuthorize("hasRole('ROLE_USER')")
   public ResponseEntity<?> getUsers() {
     return ResponseEntity.ok(userRepository.findAll());
+  }
+
+  @GetMapping("/{id}")
+  @PreAuthorize("hasRole('ROLE_USER')")
+  public ResponseEntity<?> getUser(@PathVariable long id) {
+    User user = userRepository.findById(id)
+      .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    return ResponseEntity.ok(user);
   }
 }
