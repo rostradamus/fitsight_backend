bd43924e537ffcb92479a6592b09de3af8891f36
rostradamus <rolee0429@gmail.com>
2020-05-18 13:58:14 -0700

    Revert plan

--- src/main/java/com/rostradamus/wologbackend/model/Plan.java
+++ src/main/java/com/rostradamus/wologbackend/model/Plan.java
@@ -1,12 +1,15 @@
 package com.rostradamus.wologbackend.model;
 
 
+import lombok.Data;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.util.Date;
 
 @Entity
 @Table(name = "plans")
+@Data
 public class Plan extends AuditModel {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
