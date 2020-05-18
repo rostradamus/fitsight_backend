909e54e6b89f872d066d899073aaf9192566709a
rostradamus <rolee0429@gmail.com>
2020-05-18 12:37:12 -0700

    Add skeleton for User Test

---
+++ src/test/java/com/rostradamus/wologbackend/model/UserTest.java
@@ -0,0 +1,69 @@
+package com.rostradamus.wologbackend.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+  User testUser;
+  @BeforeEach
+  void beforeEach() {
+    testUser = new User();
+  }
+
+  @Test
+  void shouldBeValid() {
+    assertNotNull(testUser);
+
+  }
+
+  @Test
+  void getId() {
+
+  }
+
+  @Test
+  void getEmail() {
+  }
+
+  @Test
+  void getPassword() {
+  }
+
+  @Test
+  void getFirstName() {
+  }
+
+  @Test
+  void getLastName() {
+  }
+
+  @Test
+  void getRoles() {
+  }
+
+  @Test
+  void setId() {
+  }
+
+  @Test
+  void setEmail() {
+  }
+
+  @Test
+  void setPassword() {
+  }
+
+  @Test
+  void setFirstName() {
+  }
+
+  @Test
+  void setLastName() {
+  }
+
+  @Test
+  void setRoles() {
+  }
+}
